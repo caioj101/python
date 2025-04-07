ac24049a1968033fef93e886f2bb4ab46926e1ac
Caio Cardoso <caioj101hub@gmail.com>
2025-04-07 19:57:33 -0300

    novos exercicios

---
+++ exercicios/ex006.py
@@ -0,0 +1,6 @@
+import math
+
+n = int(input('Digite um número: '))
+print(f'O dobro de {n} vale {n*2}.')
+print(f'O triplo de {n} vale {n*3}.')
+print(f'A raiz quadrada de {n} é igual a {math.sqrt(n):.2f}.')
