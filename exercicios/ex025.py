62e6a7fa75fb2d0cfa94eb1afab3997c4c669cdf
Caio Cardoso <caioj101hub@gmail.com>
2025-04-11 17:58:53 -0300

    exercicios do 25 ao 28

---
+++ exercicios/ex025.py
@@ -0,0 +1,2 @@
+nome = input('Qual o seu nome completo? ').strip().lower()
+print(f'Seu nome tem Silva? {'silva' in nome}')
---
+++ exercicios/ex027.py
@@ -0,0 +1,3 @@
+nome = input('Digite seu nome completo: ').strip().split()
+print(f'Seu primeiro nome é {nome[0]}')
+print(f'Seu último nome é {nome[-1]}')
